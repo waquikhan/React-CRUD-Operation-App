6ffccf7a94923dfbaf6136c2dd6d000680175389
waquikhan <103071047+waquikhan@users.noreply.github.com>
2023-09-09 23:57:55 +0530

    Added the Table and Form to adding the data from the user

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
+import React, {useState} from 'react'
 import logo from './logo.svg';
 import './App.css';
+import Form from './Form';
+import Table from './Table';
 
 function App() {
+  const [list, setList] = useState([]);
+  const [editIndex, setEditIndex] = useState(-1);
+
+  const deleteItem = (index) => {
+    const deletedList = list.filter((o, i) => i !== index);
+    setList(deletedList)
+  }
+
+
+  const addToList = (details) => {
+    if (editIndex === -1) {
+      setList([...list, details]);
+    } else {
+      const editedList = [...list];
+      editedList[editIndex] = details;
+      setList(editedList);
+      setEditIndex(-1)
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <Form list={list} editIndex={editIndex} addToList={addToList} />
+
+    <Table setEditIndex={setEditIndex} list={list} deleteItem={deleteItem} />
+      
     </div>
   );
 }
 
 export default App;
+ 
