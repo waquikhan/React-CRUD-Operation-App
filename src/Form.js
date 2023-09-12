7bd89b8898acafbb338e2f4d38a03ca838505b27
waquikhan <103071047+waquikhan@users.noreply.github.com>
2023-09-12 09:23:02 +0530

    Rename the Edit Button

--- src/Form.js
+++ src/Form.js
@@ -22,24 +22,24 @@ export default function Form(props) {
 
   return (
     <div id='Name'>
-        Name: 
-      <input type="text" value={details.name} name="name" onChange={changeDetails} />
-      <br />
-        Roll no.:
-      <input id='Roll-no' type="text" value={details.rollNo} name="rollNo" onChange={changeDetails} />
-      <br />
-        Class:
-      <input id='Class' type="text" value={details.class} name="class" onChange={changeDetails} />
-      <br />
-        Phone number:
-      <input id='phone-no' type="text" value={details.phone} name="phone" onChange={changeDetails} />
-      <br />
-        Address:
-      <input id='address' type="text" value={details.address} name="address" onChange={changeDetails} />
-      <br />
+    Name: 
+  <input type="text" value={details.name} name="name" onChange={changeDetails} />
+  <br />
+    Roll no.:
+  <input id='Roll-no' type="text" value={details.rollNo} name="rollNo" onChange={changeDetails} />
+  <br />
+    Class:
+  <input id='Class' type="text" value={details.class} name="class" onChange={changeDetails} />
+  <br />
+    Phone number:
+  <input id='phone-no' type="text" value={details.phone} name="phone" onChange={changeDetails} />
+  <br />
+    Address:
+  <input id='address' type="text" value={details.address} name="address" onChange={changeDetails} />
+  <br />
 
-      <button onClick={add} style={{ border: "1px solid black" ,color: 'white',backgroundColor:"green" }} >{props.editIndex == -1 ? 'Add': 'Edit'}</button>
+  <button onClick={add} style={{ border: "1px solid black" ,color: 'white',backgroundColor:"green" }} >{props.editIndex === -1 ? 'Add': 'update'}</button>
 
-    </div>
+</div>
   )
 }
